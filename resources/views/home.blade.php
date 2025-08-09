32fbf7051735e3b529e95e6d0685c345a049c4d4
Kotchaporn <getserious.495@gmail.com>
2025-08-09 14:52:45 +0700

    BA library commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('title', 'BA Library')
+
+@section('content')
+    <div class="container d-flex text-light justify-content-center">
+        <h1>Broken Arrow Library</h1>
+    </div>
+    <div class="container d-flex text-light justify-content-center">
+        
+    </div>
+
+@endsection
+
